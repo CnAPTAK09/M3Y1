cb37469ebb48e3504ace0102c4120aa37f8cf6cb
CnAPTAK09 <147928638+CnAPTAK09@users.noreply.github.com>
2023-10-19 21:08:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,12 +7,30 @@ class Widget(QMainWindow):
         super().__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.ui.btn_OK.clicked.connect(self.generation)
+
+    def generation(self):
+        s_number = '0123456789'
+        s_alphabet = 'qwertyuiopasdfghjklzxcvbnQWERTYUIOPASDFGHJKLZXCVBNM'
+        pas = ''
+        if self.ui.cb_number.isChecked():
+            for i in range(16):
+                pas = pas + random.choice(s_number)
+        if self.ui.cb_alphabet.isChecked():
+            for i in range(16):
+                pas = pas + random.choice(s_alphabet)
+        if self.ui.cb_number.isChecked() and self.ui.cb_alphabet.isChecked():
+            for i in range(16):
+                pas = pas + random.choice(s_number + s_alphabet)
+        self.ui.result.setText(pas)
+
+    def generation():
+        digtal = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+        letter = ['g', 'l', 'e', 'g', 'd', 'a', 'p', 'k', 'q', 's']
+        if self.ui.cb_number.isChecked():
 
 app = QApplication([])
 window = Widget()
-def generation():
-    digtal = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-    letter = ['g', 'l', 'e', 'g', 'd', 'a', 'p', 'k', 'q', 's']
-btn_OK.clicked.connect(generation)
+
 window.show()
 app.exec_()
